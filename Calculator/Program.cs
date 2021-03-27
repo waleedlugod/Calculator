b2f75c0e6ab39dec41c62b67b915eea1b4a499e1
wallrusi <lugodwaleed@gmail.com>
2021-03-27 22:02:49 +0800

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+class Calculator
+{
+    public static void Main()
+    {
+        double operandFirst, operandSecond;
+        double answer = 0;
+        char operation;
+        bool isNum;
+
+        do
+        {
+            Console.Write("Enter the first operand: ");
+            isNum = double.TryParse(Console.ReadLine(), out operandFirst);
+        } while (!isNum);
+
+        do
+        {
+            Console.Write("Enter an operation (+, -, *, /, %, ^): ");
+            operation = char.Parse(Console.ReadLine());
+        } while (operation != '+' && operation != '-' && operation != '*' && operation != '/' && operation != '%' && operation != '^');
+
+        do
+        {
+            Console.Write("Enter the second operand: ");
+            isNum = double.TryParse(Console.ReadLine(), out operandSecond);
+        } while (!isNum);
+
+        switch (operation)
+        {
+            case '+':
+                answer = operandFirst + operandSecond;
+                break;
+            case '-':
+                answer = operandFirst - operandSecond;
+                break;
+            case '*':
+                answer = operandFirst * operandSecond;
+                break;
+            case '/':
+                answer = operandFirst / operandSecond;
+                break;
+            case '%':
+                answer = operandFirst % operandSecond;
+                break;
+            case '^':
+                answer = Math.Pow(operandFirst, operandSecond);
+                break;
+        }
+
+
+        Console.WriteLine("The answer is: " + answer);
+    }
+}
